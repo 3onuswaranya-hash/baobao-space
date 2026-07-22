392032632135c42f1bed9151a3e90ce6f8f08306
Notebook <pakpoom.td@gmail.com>
2026-07-22 15:17:43 +0700

    fix(db): use TRUNCATE to completely bypass safeupdate restrictions

--- patch_change_pin.sql
+++ patch_change_pin.sql
@@ -3,10 +3,8 @@ CREATE OR REPLACE FUNCTION admin_change_pin(p_old_pin text, p_new_pin text)
 RETURNS void AS $$
 BEGIN
   IF EXISTS (SELECT 1 FROM admin_settings WHERE pin_hash = crypt(p_old_pin, pin_hash)) THEN
-    -- แพลตฟอร์ม Supabase บังคับให้ต้องมี WHERE เสมอเพื่อความปลอดภัย (Safe Update)
-    UPDATE admin_settings 
-    SET pin_hash = crypt(p_new_pin, gen_salt('bf')) 
-    WHERE pin_hash = crypt(p_old_pin, pin_hash);
+    TRUNCATE admin_settings;
+    INSERT INTO admin_settings VALUES (crypt(p_new_pin, gen_salt('bf')));
   ELSE
     RAISE EXCEPTION 'รหัสผ่านไม่ถูกต้อง';
   END IF;
